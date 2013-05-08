8c2453bb66dda40bae47b34a985f977e78df72a2
Lamont Granquist <lamont@opscode.com>
2013-05-08 11:22:26 -0700

    adding path override to bundle install

--- config/software/chef-windows.rb
+++ config/software/chef-windows.rb
@@ -102,7 +102,7 @@ build do
        "-n #{install_dir}/bin",
        "--no-rdoc --no-ri"].join(" ")
 
-  bundle "install"
+  bundle "install", :env = { "PATH" => "#{install_dir}/embedded/bin:#{ENV['PATH']}" }
 
   # gems with precompiled binaries
   gem ["install",
